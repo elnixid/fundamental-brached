a85b15f5b47f80fd8202bc190317b47f6e88da3e
Gede Wahyu <g.wahyu@gmail.com>
2022-08-08 21:30:20 +0800

    pengulangan membaca kesepuluh buku dengan for

--- perulangan_for.py
+++ perulangan_for.py
@@ -11,3 +11,7 @@ print('Ibu berkata : "Baca kesepuluh bukumu"')
 jumlah_buku_yang_sudah_dibaca = 0
 for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
     print(f"Buku ke-{jumlah_buku_yang_sudah_dibaca} dibaca")
+print("")
+nomer = 0
+for nomor in [1, 2, 3, 4]:
+    print(f"Nomor ke-{nomor}")
