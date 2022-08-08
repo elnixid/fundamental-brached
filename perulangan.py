9bf2a5ab3b5d1f8b06ae5b2caf480e1b7f95f3bf
Gede Wahyu <g.wahyu@gmail.com>
2022-08-08 16:41:50 +0800

    Soal cerita perulangan buku yang dibaca

---
+++ perulangan.py
@@ -0,0 +1,7 @@
+"""
+Implementasi Perulangan
+1. Ibu berkata : Baca kesepuluh buku baru itu
+2. Ibu berkata : Baca kesupuluh buku itu hingga paham
+Buat perulangan untuk jumlah buku yang dibaca point satu dan dua
+
+"""
