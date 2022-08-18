59e7be9c97f4204e8f9170d4f24217973c97ce48
Gede Wahyu <g.wahyu@gmail.com>
2022-08-18 21:58:42 +0800

    Mengambil data di list dengan pop, baik yang positif dan juga negatif.

--- tipe_data_list.py
+++ tipe_data_list.py
@@ -23,3 +23,33 @@ print('\nMenambahkan daftar buku dengan perintah Append')
 daftar_buku.append('Lean Start Up')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nMenghapus isi dari daftar buku dengan perintah clear()')
+daftar_buku.clear()
+print('\n----------------------')
+daftar_buku = ['Seven Habit','Blink', 'First Thinks First', 'Rich Dad']
+daftar_buku[0] = 'Delapan Habit'
+print('\nDaftar buku yang baru')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil buku yang ada di list dengan perintah pop()')
+buku_ambil = daftar_buku.pop(1)
+print('\nBuku yang tersisa di list :')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil adalah:')
+print(buku_ambil)
+
+daftar_buku = ['Seven Habit','Blink', 'First Thinks First', 'Rich Dad']
+print('\nDaftar buku yang ada:')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+    # print('\n--------------------------')
+
+print('\nMengambil data di list dengan pop minus, -1, -2, dsb')
+daftar_buku.pop(-1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
